b99fc82efe657894f9c5c988e4f6f56869201329
EC2 Default User <ec2-user@ip-172-31-10-40.ec2.internal>
2021-02-05 00:11:12 +0000

    Modified app.py, getting 10 top tracks from artist

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import random
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv()) # This is to load your API keys from .env
@@ -26,17 +27,28 @@ headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
+#Artist ID array
+#Bad bunny, Maroon 5, Travis Scott 
+artist_ID = ['4q3ewBCX7sLwd24euuV69X', '04gDigrS5kc9YWfZHwBETP',
+              '0Y5tJX1MQlPlqiwlOH1tJY']
+#Choose random artist ID
+random_num = random.randint(0, 2)
+my_artistID = artist_ID[random_num]
+
 # base URL of all Spotify API endpoints - browse API
-BASE_URL = 'https://api.spotify.com/v1/browse/new-releases'
+#BASE_URL = 'https://api.spotify.com/v1/artists/{id}/top-tracks'
+
+BASE_URL = 'https://api.spotify.com/v1/artists/' + my_artistID + '/top-tracks'
 
 # actual GET request with proper header
 response = requests.get(BASE_URL, 
-                headers=headers, 
-                params={'limit': 10})
+                headers=headers,
+                params={'market': 'US'})
 
 #array of data from response
 data = response.json()
+#print(data)
 
 #for loop to get 10 songs
 for i in range(0,10):
-    print(data['albums']['items'][i]['name'])
+    print(data['tracks'][i]['name'])
